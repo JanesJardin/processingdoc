6b99f7842d17330bd417b5a53d618ff01f07169f
Janesjardin <janesjardin@hotmaail.com>
2018-04-16 00:30:45 +0800

    add temple csv file

--- src/main/java/com/interview/processingdoc/provider/PreSrcDoc.java
+++ src/main/java/com/interview/processingdoc/provider/PreSrcDoc.java
@@ -12,14 +12,16 @@ import java.util.List;
  * @Date: 18-4-14
  */
 public class PreSrcDoc {
+    private static String CSV ="csv";
     /**
      * 形成待处理文档
+     *
      * @param dataFilePath 数据文件存放文件夹路径
      * @return 待处理文档列表
      */
-    public static List<PendingDocDto> makeDoc(String dataFilePath ){
-        File f=new File(dataFilePath);
-        if(!f.exists()){
+    public static List<PendingDocDto> makeDoc(String dataFilePath) {
+        File f = new File(dataFilePath);
+        if (!f.exists()) {
             try {
                 throw new Exception("data file path not exist!");
             } catch (Exception e) {
@@ -28,10 +30,14 @@ public class PreSrcDoc {
         }
         File[] files = f.listFiles();
         List<PendingDocDto> docList = new ArrayList<>();//文档列表
-        for(File file:files){
-            if(!file.isDirectory()) {
-                PendingDocDto pendingDocVo = new PendingDocDto(file.getName(),file.getAbsolutePath());
-                docList.add(pendingDocVo);
+        for (File file : files) {
+            if (!file.isDirectory()) {
+                String fileName = file.getName();
+                String suffix = fileName.substring(fileName.lastIndexOf(".") + 1);
+                if(suffix.equals(CSV)) {
+                    PendingDocDto pendingDocVo = new PendingDocDto(fileName, file.getAbsolutePath());
+                    docList.add(pendingDocVo);
+                }
             }
         }
         return docList;
